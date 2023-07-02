d143d9f31868a9ae12ad553b26bb27e7f6dfc5ca
Chung <ourour0218@gmail.com>
2023-07-02 20:14:20 +0800

    calculate avg length of reviews

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -6,5 +6,12 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0: # %是求餘數
 			print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+
+#算出評論的平均長度
+sum_len = 0
+
+for d in data:
+	sum_len = sum_len + len(d)
+print('每筆留言平均長度為:', sum_len/len(data), '個字')
