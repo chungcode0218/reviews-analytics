65e8a89223bd74680a28be1345d25c0411b0c1fc
Chung <ourour0218@gmail.com>
2023-08-01 00:05:01 +0800

    add word count function

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -8,6 +8,7 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
+print(data[0])
 
 #算出評論的平均長度
 sum_len = 0
@@ -31,3 +32,48 @@ for d in data:
 	if 'service' in d:
 		service.append(d)
 print('一共有', len(service), '筆留言有提到"服務"')
+
+#文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split('')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1281400:
+		print(word, ':', wc[word])
+print(len(wc))
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為:', wc[word])
+	else:
+		print('這個字沒有出現過喔')
+print('感謝使用查詢功能')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
